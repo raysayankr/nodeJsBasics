b5197cea25fb5e161a6b33bc09e1ee26d81d0028
sayan771993 <sayan771993@gmail.com>
2019-06-06 00:31:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const tutorial = require('./tutorial');
+console.log(tutorial);
+console.log(tutorial.sum(5,8));
+console.log(tutorial.pi);
+console.log(new tutorial.SomeMathObject());
+
+const eventEmitter =require('event');
